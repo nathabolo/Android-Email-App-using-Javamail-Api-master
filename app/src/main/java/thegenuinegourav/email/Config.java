c0d0b19e017fda5bab6a3398b97fd07f3e71b1ce
Admin Mini <adminmini@Admins-Mac-mini.local>
2018-04-21 10:21:50 +0200

    Initial commit

---
+++ app/src/main/java/thegenuinegourav/email/Config.java
@@ -0,0 +1,6 @@
+package thegenuinegourav.email;
+
+public class Config {
+    public static final String EMAIL ="malizomamabolo@gmail.com"; //your-gmail-username
+    public static final String PASSWORD ="33nathabolo23nm"; //your-gmail-password
+}
